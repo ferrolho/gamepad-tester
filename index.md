5bafec13a0e12a6624ee7d7305f2b7ec20e83b45
Henrique Ferrolho <henriqueferrolho@gmail.com>
2018-01-11 23:56:15 +0000

    Adds assets for visual feedback of controller usage

--- index.md
+++ index.md
@@ -5,7 +5,13 @@
 layout: home
 ---
 
+<link rel="stylesheet" type="text/css" href="{{ '/assets/custom.css' | absolute_url }}">
+
 <span id="helper-assets-path" hidden>{{ '/assets' | absolute_url }}</span>
+<div id="img-controller-overlays">
+	<img id="stick_axis_left" src="{{ '/assets/xbox-one-s-wireless-controller-stick_axis_left.png' | absolute_url }}">
+	<img id="stick_axis_right" src="{{ '/assets/xbox-one-s-wireless-controller-stick_axis_right.png' | absolute_url }}">
+</div>
 <img id="img-controller" src="{{ '/assets/xbox-one-s-wireless-controller.png' | absolute_url }}">
 
 <script
--- js/app.js
+++ js/app.js
@@ -1,11 +1,51 @@
-const gamepad = new Gamepad();
+/* global $, Gamepad */
 
-gamepad.on('press', 'button_1', e => {
-    console.log(`${e.button} was pressed!`);
-    $('#img-controller').attr('src', `${$('#helper-assets-path').html()}/xbox-one-s-wireless-controller-a.png`)
-});
+const gamepad = new Gamepad()
 
-gamepad.on('release', 'button_1', e => {
-    console.log(`${e.button} was pressed!`);
-    $('#img-controller').attr('src', `${$('#helper-assets-path').html()}/xbox-one-s-wireless-controller.png`)
-});
+const gamepadButtons = [
+  'button_1', 'button_2', 'button_3', 'button_4',
+  'shoulder_top_left', 'shoulder_top_right',
+  'select', 'start', 'vendor',
+  'd_pad_up', 'd_pad_down', 'd_pad_left', 'd_pad_right'
+]
+
+for (const button of gamepadButtons) {
+  gamepad.on('press', button, e => {
+    $('#img-controller-overlays').append(`<img id="${button}" src="${$('#helper-assets-path').html()}/xbox-one-s-wireless-controller-${button}.png" />`)
+  })
+
+  gamepad.on('release', button, e => {
+    $('img').remove(`#${button}`)
+  })
+}
+
+const gamepadStickButtons = [ 'stick_button_left', 'stick_button_right' ]
+
+for (const button of gamepadStickButtons) {
+  gamepad.on('press', button, e => {
+    $('img').remove(`#stick_axis_${button.split('_').pop()}`)
+    $('#img-controller-overlays').append(`<img id="${button}" src="${$('#helper-assets-path').html()}/xbox-one-s-wireless-controller-${button}.png" />`)
+  })
+
+  gamepad.on('release', button, e => {
+    $('img').remove(`#${button}`)
+    $('#img-controller-overlays').append(`<img id="stick_axis_${button.split('_').pop()}" src="${$('#helper-assets-path').html()}/xbox-one-s-wireless-controller-stick_axis_${button.split('_').pop()}.png" />`)
+  })
+}
+
+gamepad.on('connect', e => {
+  console.log(`Controller ${e.index} connected!`)
+  console.log(navigator.getGamepads()[e.index].id.toLowerCase().indexOf('xbox') !== -1) // it is an xbox controller
+})
+
+gamepad.on('disconnect', e => {
+  console.log(`Controller ${e.index} disconnected!`)
+})
+
+gamepad.on('hold', 'stick_button_left', e => {
+  console.log(`stick_button_left has a value of ${e.value}!`)
+})
+
+gamepad.on('hold', 'stick_axis_left', e => {
+  console.log(`stick_axis_left has a value of ${e.value}!`)
+})
